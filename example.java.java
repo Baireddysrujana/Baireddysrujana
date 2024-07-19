f11e15b3a2ee3bd495f596a16686152f3f9e6cf0
Baireddysrujana <srujanarddy3@gmail.com>
2024-07-19 19:31:06 -0400

    feture3

---
+++ example.java.java
@@ -0,0 +1,16 @@
+class IfElseLadderDemo{
+public static void main (string[] args)
+{
+int a =-3
+If(a>0){
+system.out.println("a is positive number");
+}
+else if(a<0){
+system.out.println("a is negative number");
+}
+else{
+system.out.println("a is zero");
+}
+}
+}
+
